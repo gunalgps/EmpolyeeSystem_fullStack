f3bb7ac8f389c22eff2c678395ff0bf73ad3fc1d
gunalgps <gunalpalani1@gmail.com>
2026-07-23 04:19:30 +0530

    front enned frist chnage

---
+++ ems-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListEmployeeComponent from './Compents/listemployeecompent.jsx'
+
+function App() {
+  return  (
+    <>
+      <ListEmployeeComponent />
+    </>
+  )
+}
+
+export default App
+
---
+++ ems-frontend/src/helloworld.jsx
@@ -0,0 +1,4 @@
+function HelloWorld() {
+  return <h1 className="text-uppercase">hello world</h1>
+}
+export default HelloWorld
